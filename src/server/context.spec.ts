fbee3655c5086c9da4a2e43df8ced0dc9831b9e0
Kat Schelonka <kschelonka@mozilla.com>
2022-01-26 16:23:27 -0800

    test: demonstrate dataloader bug

--- src/server/context.spec.ts
+++ src/server/context.spec.ts
@@ -19,38 +19,17 @@ describe('context', () => {
       givenUrl: 'dont-care.com',
     },
   };
+  let batchQueryFnSpy;
+
+  beforeEach(() => {
+    batchQueryFnSpy =
+      SavedItemDataService.prototype.batchGetSavedItemsByGivenUrls = jest
+        .fn()
+        .mockResolvedValue([savedItem]);
+  });
   afterEach(() => jest.clearAllMocks());
 
   it('creates a data loader for saved items on initialization', async () => {
-    const batchQueryFnSpy =
-      (SavedItemDataService.prototype.batchGetSavedItemsByGivenIds = jest
-        .fn()
-        .mockResolvedValue([savedItem]));
-
-    const context = new ContextManager({
-      request: {
-        headers: { userid: '1', apiid: '0' },
-      },
-      db: {
-        readClient: jest.fn() as unknown as Knex,
-        writeClient: jest.fn() as unknown as Knex,
-      },
-      eventEmitter: null,
-    });
-
-    const savedItems = await context.dataLoaders.savedItemsById.load('1');
-
-    expect(context.dataLoaders.savedItemsById).to.be.instanceof(DataLoader);
-    expect(batchQueryFnSpy.mock.calls[0][0]).to.deep.equal(['1']);
-    expect(savedItems).to.deep.equal(savedItem);
-  });
-
-  it('creates a data loader for saved items on initialization', async () => {
-    const batchQueryFnSpy =
-      (SavedItemDataService.prototype.batchGetSavedItemsByGivenUrls = jest
-        .fn()
-        .mockResolvedValue([savedItem]));
-
     const context = new ContextManager({
       request: {
         headers: { userid: '1', apiid: '0' },
@@ -69,5 +48,32 @@ describe('context', () => {
     expect(context.dataLoaders.savedItemsByUrl).to.be.instanceof(DataLoader);
     expect(batchQueryFnSpy.mock.calls[0][0]).to.deep.equal(['dont-care.com']);
     expect(savedItems).to.deep.equal(savedItem);
+  });
+  it('Uses the same dataloader for every load request', async () => {
+    const context = new ContextManager({
+      request: {
+        headers: { userid: '1', apiid: '0' },
+      },
+      db: {
+        readClient: jest.fn() as unknown as Knex,
+        writeClient: jest.fn() as unknown as Knex,
+      },
+      eventEmitter: null,
+    });
+
+    // Referencing the loader 2x should return the same object
+    const loader = context.dataLoaders.savedItems;
+    const loaderAgain = context.dataLoaders.savedItems;
+    await loader.load('dont-care.com');
+    // At this point both loaders should have filled cache since referencing same object
+    expect(Array.from((loader as any)._cacheMap.keys())).to.contain(
+      'dont-care.com'
+    );
+    expect(Array.from((loaderAgain as any)._cacheMap.keys())).to.contain(
+      'dont-care.com'
+    );
+    await loaderAgain.load('dont-care.com');
+    // Second load should have used the cache, so only one call to batch fn
+    expect(batchQueryFnSpy.mock.calls.length).to.equal(1);
   });
 });
