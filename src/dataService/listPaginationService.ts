733e7c787cd5b4f165d9af030e08449942683727
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2022-03-10 08:18:28 -0800

    bugfix(savedItems): do not throw error if filter returns no result (#197)

--- src/dataService/listPaginationService.ts
+++ src/dataService/listPaginationService.ts
@@ -620,8 +620,10 @@ export class ListPaginationService {
         cursor: this.encodeCursor(node.id, node[sortColumn]),
       };
     });
-    pageInfo['startCursor'] = edges[0].cursor;
-    pageInfo['endCursor'] = edges[edges.length - 1].cursor;
+    if (edges.length) {
+      pageInfo['startCursor'] = edges[0].cursor;
+      pageInfo['endCursor'] = edges[edges.length - 1].cursor;
+    }
     return {
       edges,
       pageInfo,
@@ -632,7 +634,7 @@ export class ListPaginationService {
     pagedResult: ListEntity[],
     pagination: PaginationInput
   ) {
-    const pageInfo = {};
+    const pageInfo = { startCursor: null, endCursor: null };
     if (pagination.first) {
       pageInfo['hasNextPage'] = pagedResult.length > pagination.first;
       if (pagination.after) {
