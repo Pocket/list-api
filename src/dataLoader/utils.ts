2fcb43dbef7e075efe28b9c0f5a626997fc01d2c
Kat Schelonka <kschelonka@mozilla.com>
2022-05-03 17:49:03 -0700

    feat: utility function for reordering data

---
+++ src/dataLoader/utils.ts
@@ -0,0 +1,17 @@
+// Ensure only an indexible type on the object is used for reordering results,
+// and that the key matches
+type ReorderMap<T, K extends keyof T> = {
+  key: K;
+  values: T[K] extends string | number | symbol ? T[K][] : never;
+};
+
+export function reorderResultByKey<T, K extends keyof T>(
+  reorderMap: ReorderMap<T, K>,
+  results: T[]
+) {
+  const resMap = results.reduce((acc, element) => {
+    acc[element[reorderMap.key]] = element;
+    return acc;
+  }, {} as any); // idk... help me with this index type
+  return reorderMap.values.map((input) => resMap[input]);
+}
