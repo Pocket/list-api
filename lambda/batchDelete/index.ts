f9f743e641dce8333c9bb1a2e8c157307cee9454
Kelvin Yeboah <kyeboah@getpocket.com>
2022-05-20 14:23:46 -0700

    fix: fix invalid packaged lambda file path

--- lambda/batchDelete/index.ts
+++ lambda/batchDelete/index.ts
@@ -2,7 +2,7 @@ import { SQSEvent } from 'aws-lambda';
 import * as Sentry from '@sentry/serverless';
 
 function processor(event: SQSEvent) {
-  console.log(`Lambda invoked with: ${JSON.stringify(event)}`);
+  console.log(`Batch delete Lambda invoked with: ${JSON.stringify(event)}`);
 }
 
 export const handler = Sentry.AWSLambda.wrapHandler(processor);
