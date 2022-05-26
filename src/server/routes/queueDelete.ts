336afae6a877eef88544cb75061aba851c93b6e7
Kelvin Yeboah <kyeboah@getpocket.com>
2022-05-26 10:36:36 -0700

    fix: uniqur traceId per message

--- src/server/routes/queueDelete.ts
+++ src/server/routes/queueDelete.ts
@@ -104,7 +104,7 @@ export async function enqueueSavedItemIds(
           email,
           status,
           itemIds: nextChunk.value,
-          traceId: traceId,
+          traceId: nanoid(),
         })
       );
 
