fd0966309342b25477b727329d5cd808f59aa270
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2022-12-16 09:29:22 -0800

    feat(deps): Migrate to apollo v4 [INFRA-842] (#599)
    * chore: update imports and reorganize tests
    
    * more v4 updates; server runs
    
    * updates so can use _startServer in e2e tests
    
    * updated an example test to go through e2e with supertest
    
    * chore: update tests for v4 e2e calls
    
    * don't submit usage reporting from subgraph
    
    Got a warning it was disabled automatically.
    This should be handled by gateway.
    
    * fix variable name collision

--- src/models/tag.ts
+++ src/models/tag.ts
@@ -12,9 +12,7 @@ import {
 import config from '../config';
 import { IContext } from '../server/context';
 import { SavedItemDataService, TagDataService } from '../dataService';
-import { UserInputError } from 'apollo-server-express';
-import { ApolloError } from 'apollo-server-errors';
-import { NotFoundError } from '@pocket-tools/apollo-utils';
+import { NotFoundError, UserInputError } from '@pocket-tools/apollo-utils';
 import { addslashes } from 'locutus/php/strings';
 import * as Sentry from '@sentry/node';
 
@@ -300,7 +298,7 @@ const validateTag = (tag: any): true => {
 
   if (err) {
     Sentry.captureException(err);
-    throw new ApolloError(err);
+    throw new Error(err);
   }
   return true;
 };
