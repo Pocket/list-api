713f20c68bc8e71688db66925a51326d144d7262
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2023-02-01 15:55:19 -0800

    Revert "Revert "Code Freeze 2022 Thaw"" (#615)
    * Revert "Revert "feat(deps): Migrate to apollo v4 [INFRA-842] (#599) (#607)" (#614)"
    
    This reverts commit 8e272808ae1330ed30731b46d1b7d0f04eb59386.
    
    * chore: re-expose health check url
    
    fixes merge conflict

---
+++ src/server/apollo.ts
@@ -0,0 +1,102 @@
+import * as Sentry from '@sentry/node';
+import express from 'express';
+import http from 'http';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
+import {
+  ApolloServerPluginLandingPageDisabled,
+  ApolloServerPluginUsageReportingDisabled,
+} from '@apollo/server/plugin/disabled';
+import { sentryPlugin, errorHandler } from '@pocket-tools/apollo-utils';
+import config from '../config';
+import { ContextManager } from './context';
+import { readClient } from '../database/client';
+import {
+  initItemEventHandlers,
+  itemsEventEmitter,
+  snowplowEventHandler,
+  sqsEventHandler,
+  unifiedEventHandler,
+} from '../businessEvents';
+import queueDeleteRouter from './routes/queueDelete';
+import { BatchDeleteHandler } from '../aws/batchDeleteHandler';
+import { EventEmitter } from 'events';
+import { initAccountDeletionCompleteEvents } from '../aws/eventTypes';
+import { typeDefs } from './typeDefs';
+import { resolvers } from '../resolvers';
+
+export async function startServer(port: number) {
+  Sentry.init({
+    ...config.sentry,
+    debug: config.sentry.environment == 'development',
+  });
+
+  const app = express();
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
+  const httpServer = http.createServer(app);
+  // Initialize routes
+  app.use('/queueDelete', queueDeleteRouter);
+
+  // Expose health check url
+  app.get('/.well-known/apollo/server-health', (req, res) => {
+    res.status(200).send('ok');
+  });
+
+  // Start BatchDelete queue polling if not test env
+  if (process.env.NODE_ENV != 'test') {
+    new BatchDeleteHandler(new EventEmitter());
+  }
+
+  // Initialize event handlers
+  initItemEventHandlers(itemsEventEmitter, [
+    unifiedEventHandler,
+    sqsEventHandler,
+    snowplowEventHandler,
+    initAccountDeletionCompleteEvents,
+  ]);
+
+  // Inject initialized event emittter to create context factory function
+  const contextFactory = (req: express.Request) => {
+    return new ContextManager({
+      request: req,
+      dbClient: readClient(),
+      eventEmitter: itemsEventEmitter,
+    });
+  };
+
+  const server = new ApolloServer<ContextManager>({
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+    plugins: [
+      sentryPlugin,
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageDisabled()
+        : ApolloServerPluginLandingPageGraphQLPlayground(),
+      ApolloServerPluginUsageReportingDisabled(),
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+    ],
+    formatError: process.env.NODE_ENV !== 'test' ? errorHandler : undefined,
+    introspection: process.env.NODE_ENV !== 'production',
+  });
+
+  await server.start();
+
+  // Apply to root
+  const url = '/';
+
+  app.use(
+    url,
+    // JSON parser to enable POST body with JSON
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => contextFactory(req),
+    })
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+  return { app, server, url };
+}
