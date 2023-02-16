b9ca5d68acdc2df0e8c866ef0835063feffa79b3
Kat Schelonka <kschelonka@mozilla.com>
2023-02-16 10:10:51 -0800

    feat: shared model for resolving error paths

---
+++ src/models/baseError.ts
@@ -0,0 +1,24 @@
+import { GraphQLResolveInfo } from 'graphql';
+
+export class BaseErrorModel {
+  /**
+   * Stringify the path data; traverses path ancestors
+   * and prepends field names with period
+   * @param path The path from the Info object passed to all resolvers
+   * @returns a period-separated string of the path to the field
+   * (direct ancestors only)
+   */
+  public path(path: GraphQLResolveInfo['path']): string {
+    let pathNode = path;
+    // Extract current field node, then every previous node
+    let pathString = pathNode.key.toString();
+    // This could be done recursively but usually better not to
+    // in javascript
+    while (pathNode.prev !== undefined) {
+      // period-separated path
+      pathString = pathNode.prev.key.toString() + '.' + pathString;
+      pathNode = pathNode.prev;
+    }
+    return pathString;
+  }
+}
