a0728a7956142ed9f2ec60103aef3ba0ea174891
Sri Kirsh <skirsh@mozilla.com>
2023-05-10 16:12:32 -0700

    chore(account-deletion): remove user-events subscription (for account-deletion) (#748)
    * remove user-events subscription (for account-deletion)
    
    * lint fix

--- .aws/src/lambda/EventLambda.ts
+++ .aws/src/lambda/EventLambda.ts
@@ -1,8 +1,5 @@
-import { SqsQueue } from '@cdktf/provider-aws/lib/sqs-queue';
-import { ApplicationSqsSnsTopicSubscription } from '@pocket-tools/terraform-modules';
 import { Construct } from 'constructs';
 import { SqsLambda, SqsLambdaProps } from './base/SqsLambda';
-import { config as stackConfig } from '../config';
 
 export class EventLambda extends Construct {
   constructor(
@@ -12,22 +9,9 @@ export class EventLambda extends Construct {
   ) {
     super(scope, name.toLowerCase());
 
-    const sqsLambda = new SqsLambda(this, 'Sqs-Event-Consumer', {
+    new SqsLambda(this, 'Sqs-Event-Consumer', {
       vpc: config.vpc,
       batchSize: 10,
     });
-    const lambda = sqsLambda.lambda;
-
-    new ApplicationSqsSnsTopicSubscription(
-      this,
-      'user-events-sns-subscription',
-      {
-        name: stackConfig.prefix,
-        snsTopicArn: `arn:aws:sns:${config.vpc.region}:${config.vpc.accountId}:${stackConfig.lambda.snsTopicName.userEvents}`,
-        sqsQueue: lambda.sqsQueueResource,
-        tags: stackConfig.tags,
-        dependsOn: [lambda.sqsQueueResource as SqsQueue],
-      }
-    );
   }
 }
