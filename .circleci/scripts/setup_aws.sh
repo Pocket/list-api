24ad90b542e52bd30769a4641cb8151c29ec4499
Daniel Brooks <bass_rock@me.com>
2023-07-18 08:57:35 -0700

    Update setup_aws.sh

--- .circleci/scripts/setup_aws.sh
+++ .circleci/scripts/setup_aws.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo apt-get update && sudo apt-get install -y python3-pip
-pip3 install boto3==1.26.123 awscli-local awscli==1.27.123
+pip3 install boto3==1.26.123 awscli-local awscli==1.27.123 --no-build-isolation
 
 
 for Script in .docker/localstack/*.sh ; do
